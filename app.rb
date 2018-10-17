4405c951a75090ea264fb57bccd23d4a82313288
Bill Muxworthy <bill_muxworthy@hotmail.com>
2018-10-17 15:50:37 +0100

    added get '/play' to ruby file

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-
   post '/names' do
-    @player_one = params[:player_one]
-    @player_two = params[:player_two]
+    session[:player_one] = params[:player_one]
+    session[:player_two] = params[:player_two]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one = session[:player_one]
+    @player_two = session[:player_two]
     erb :play
   end
+
+  run! if app_file == $0
 end
